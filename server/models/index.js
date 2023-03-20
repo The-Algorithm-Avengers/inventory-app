5203c12a1d08c865c8c407a9b8b3b39342f1a3b7
Johnny Chen <Johnnychen384@gmail.com>
2023-03-20 13:53:53 -0400

    changed sauces model to items

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,15 @@
 const {Sequelize} = require('sequelize')
 const {sequelize} = require('../db')
 
-const Sauce = sequelize.define("sauces", {
+const Item = sequelize.define("items", {
   name: Sequelize.STRING,
+  description: Sequelize.STRING,
+  price: Sequelize.INTEGER,
+  category: Sequelize.STRING,
   image: Sequelize.STRING,
 });
 
 module.exports = {
   db: sequelize,
-  Sauce,
+  Item,
 };
