39b7f74a2a835fe008e9b071882e63eeba020b70
kevin-liu <liu.kevin1997@gmail.com>
2023-03-20 12:45:01 -0400

    added my name

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -4,6 +4,7 @@ import { SaucesList } from './SaucesList';
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
+
 export const App = () => {
 
 	const [sauces, setSauces] = useState([]);
@@ -26,6 +27,7 @@ export const App = () => {
 	return (
 		<main>	
       <h1>Sauce Store</h1>
+			<h2>kevin</h2>
 			<h2>All things 🔥</h2>
 			<SaucesList sauces={sauces} />
 		</main>
