fe4441231f642b958415216fe15fbacf9bd34598
kevin-liu <liu.kevin1997@gmail.com>
2023-03-20 16:10:40 -0400

    create a item route done

--- server/routes/Items.js
+++ server/routes/Items.js
@@ -1,31 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const { Item } = require("../models");
+const { Item } = require("../models/index");
 
 // GET /items
 router.get("/", async (req, res, next) => {
-  try {
-    const items = await Item.findAll();
-    res.send(items);
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const items = await Item.findAll();
+		res.send(items);
+    console.log('hi')
+	} catch (error) {
+    console.log('heloo')
+		next(error);
+	}
 });
 
 // GET /items/:title
-router.get("/:title", async (req, res) => {
+router.get("/:id", async (req, res) => {
+	try {
+    const item = await Item.findByPk(req.params.id)
+		res.status(200).json(item);
+	} catch (error) {
+		console.error("There is a problem with getting a single item" + error);
+		res.status(500).send(error);
+	}
+});
 
-  try {
-    const item = await Item.findOne({
-      where: {title: req.params.title}
-    })
-    res.status(200).json(item)
-
-  } catch (error) {
-    console.error("There is a problem with getting a single item" + error)
-    res.status(500).send(error)
-  }
-
-})
+router.post("/", async (req, res) => {
+	try {
+    const singleItem = await Item.create(req.body)
+		res.status(200).json(singleItem);
+	} catch (error) {
+		console.error("Problem with creating an item" + error);
+		res.status(500).send(error);
+	}
+});
 
 module.exports = router;
