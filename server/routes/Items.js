05057a9b8784ecc500a0db5db4df887c110e4b97
kevin-liu <liu.kevin1997@gmail.com>
2023-03-20 15:49:42 -0400

    test

--- server/routes/Items.js
+++ server/routes/Items.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Item } = require("../models");
 
-// GET /sauce
+// GET /items
 router.get("/", async (req, res, next) => {
   try {
     const items = await Item.findAll();
