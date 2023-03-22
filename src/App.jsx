a743b1cc05504557406910523e45edf4c04c33f6
kevin-liu <liu.kevin1997@gmail.com>
2023-03-22 13:45:29 -0400

    deletes duplicate appjs

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import Footer from "./components/Footer";
 import ItemsList from "./components/ItemsList";
 import apiURL from "./utils/api";
 import { CreateForm } from "./components/CreateForm";
-import {Detail} from "./components/Details"
 
 const App = () => {
   const [items, setItems] = useState([]);
-  const [showDetails, setShowDetails] = useState(false)
-  const [targetItem , setTargetItem] = useState({})
+  const [addItem, setAddItem] = useState(false)
 
   async function fetchItems() {
     try {
@@ -36,23 +34,12 @@ const App = () => {
         }
     })
       fetchItems()
-      setShowDetails(false)
 
     } catch (error) {
       console.log("Could not delete item" + error)
     }
     
-  }
 
-  const getItem = async (id) => {
-    try {
-      const res = await fetch(`${apiURL}/items/${id}`)
-      const data = await res.json()
-      setTargetItem(data)
-      setShowDetails(true)
-    } catch (error) {
-      console.log("Couldnt get item" + error)
-    }
   }
 
   useEffect(() => {
@@ -64,22 +51,11 @@ const App = () => {
       <div>
         <main>
           <Header />
-
-          {!showDetails ?
-          <>
-            <h1>Items Store</h1>
-            <h2>All things 🔥</h2>
-            <ItemsList items={items} getItem={getItem}/>
-            <CreateForm/>
-          </> :
-
-          <Detail deleteItem={deleteItem} item={targetItem} setShowDetails={setShowDetails}/>
-        
-        }
-
-          
+          <h1>Items Store</h1>
+          <h2>All things 🔥</h2>
+          <ItemsList items={items} />
+          <CreateForm/>
         </main>
-
         <Footer />
       </div>
     </>
