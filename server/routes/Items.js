52f67a2bf2a621b5edbca4869e353a83f867015e
Johnny Chen <Johnnychen384@gmail.com>
2023-03-22 16:37:55 -0400

    cleaned up

--- server/routes/Items.js
+++ server/routes/Items.js
@@ -80,7 +80,7 @@ router.put(
       if (!item) {
         return res.status(404).json({ error: "Item not found" });
       }
-	  
+
       const updatedItem = await item.update(req.body);
       res.status(200).json(updatedItem);
     } catch (error) {
