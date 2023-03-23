7af8abbe06472aaab8cc116b1df6d025b67d89f7
romanprotoliuk <romanprotoliuk@gmail.com>
2023-03-23 11:08:58 -0700

    filtering func through search, minor styling fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,113 +9,135 @@ import { Detail } from "./components/Details";
 import { UpdateForm } from "./components/UpdateForm";
 
 const App = () => {
-	const [items, setItems] = useState([]);
-	const [showDetails, setShowDetails] = useState(false);
-	const [targetItem, setTargetItem] = useState({});
-	const [showForm, setShowForm] = useState(false);
-	const [editForm, setEditForm] = useState(false);
+  const [items, setItems] = useState([]);
+  const [showDetails, setShowDetails] = useState(false);
+  const [targetItem, setTargetItem] = useState({});
+  const [showForm, setShowForm] = useState(false);
+  const [editForm, setEditForm] = useState(false);
+  const [titleSearch, setTitleSearch] = useState("");
 
-	// Gets all Items and sets it to items State
-	async function fetchItems() {
-		try {
-			const response = await fetch(`${apiURL}/items`);
+  async function fetchItems() {
+    try {
+      const response = await fetch(`${apiURL}/items`);
 
-			if (!response.ok) {
-				throw new Error(`HTTP error! Status: ${response.status}`);
-			}
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
 
-			const itemsData = await response.json();
-			setItems(itemsData);
-		} catch (err) {
-			console.log("Oh no an error! ", err);
-		}
-	}
+      const itemsData = await response.json();
+      setItems(itemsData);
+    } catch (err) {
+      console.log("Oh no an error! ", err);
+    }
+  }
 
-	// Deletes a single item by ID, reruns fetchItems and by setting showDetails to false stops rendering Details component
-	const deleteItem = async (id) => {
-		try {
-			await fetch(`${apiURL}/items/${id}`, {
-				method: "DELETE",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			fetchItems();
-			setShowDetails(false);
-		} catch (error) {
-			console.log("Could not delete item" + error);
-		}
-	};
+  const deleteItem = async (id) => {
+    try {
+      await fetch(`${apiURL}/items/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      fetchItems();
+      setShowDetails(false);
+    } catch (error) {
+      console.log("Could not delete item" + error);
+    }
+  };
 
-	// Fetches a single item from DB based on ID passed in && set that item to targetItem state && setting showDetails state to true to render Details component.
-	const getItem = async (id) => {
-		try {
-			const res = await fetch(`${apiURL}/items/${id}`);
-			const data = await res.json();
-			setTargetItem(data);
-			setShowDetails(true);
-		} catch (error) {
-			console.log("Couldn't get item" + error);
-		}
-	};
+  const getItem = async (id) => {
+    try {
+      const res = await fetch(`${apiURL}/items/${id}`);
+      const data = await res.json();
+      setTargetItem(data);
+      setShowDetails(true);
+    } catch (error) {
+      console.log("Couldn't get item" + error);
+    }
+  };
 
-	// On start up run fetchItems ONCE.
-	useEffect(() => {
-		fetchItems();
-	}, []);
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-	return (
-		<>
-			<Header />
-			<main>
-				{/* If showDetails is false */}
-				{!showDetails ? (
-					<>
-						{/* If showDetails is false && showForm is false */}
-						{!showForm ? (
-							<>
-								<div className="buttonDiv">
-									<button
-										className="itemButton"
-										onClick={() => setShowForm(!showForm)}
-									>
-										ADD ITEM
-									</button>
-								</div>
-								<h2 className="header-subtitle">Items Store</h2>
-								<ItemsList items={items} getItem={getItem} />
-							</>
-						) : (
-							// If showDetails is false && showForm is true
-							<CreateForm setShowForm={setShowForm} fetchItems={fetchItems} />
-						)}
-					</>
-				) : (
-					<>
-						{/* If showDetails is true */}
-						{!editForm ? (
-							// if showDetails is true && editForm is false
-							<Detail
-								deleteItem={deleteItem}
-								item={targetItem}
-								setShowDetails={setShowDetails}
-								setEditForm={setEditForm}
-							/>
-						) : (
-							// if showDetails is true && editForm is true
-							<UpdateForm
-								item={targetItem}
-								setEditForm={setEditForm}
-								fetchItems={fetchItems}
-								setShowDetails={setShowDetails}
-							/>
-						)}
-					</>
-				)}
-			</main>
-			<Footer />
-		</>
-	);
+  const handleChange = (e) => {
+    setTitleSearch(e.target.value);
+  };
+
+  const getFilteredPosition = () => {
+    let newSearchTerm = titleSearch.toLowerCase();
+    return items.filter((v) => {
+      let lowerCaseName = v.title.toLowerCase();
+      return lowerCaseName.includes(newSearchTerm);
+    });
+  };
+
+  const filteredTitles = getFilteredPosition();
+
+  return (
+    <>
+      <Header />
+
+      <main>
+        {/* If showDetails is false */}
+        {!showDetails ? (
+          <>
+            {/* If showDetails is false && showForm is false */}
+            {!showForm ? (
+              <>
+                <div className="search-add-item-container">
+                  <input
+                    className="searchInput"
+                    placeholder="Search for items"
+                    type="text"
+                    onChange={handleChange}
+                    value={titleSearch}
+                  />
+
+                  {/* <div className="buttonDiv"> */}
+                  <button
+                    className="itemButton"
+                    onClick={() => setShowForm(!showForm)}
+                  >
+                    ADD ITEM
+                  </button>
+                  {/* </div> */}
+                </div>
+                <h2 className="header-subtitle">Items Store</h2>
+                <ItemsList items={filteredTitles} getItem={getItem} />
+              </>
+            ) : (
+              // If showDetails is false && showForm is true
+              <CreateForm setShowForm={setShowForm} fetchItems={fetchItems} />
+            )}
+          </>
+        ) : (
+          <>
+            {/* If showDetails is true */}
+            {!editForm ? (
+              // if showDetails is true && editForm is false
+              <Detail
+                deleteItem={deleteItem}
+                item={targetItem}
+                setShowDetails={setShowDetails}
+                setEditForm={setEditForm}
+              />
+            ) : (
+              // if showDetails is true && editForm is true
+              <UpdateForm
+                item={targetItem}
+                setEditForm={setEditForm}
+                fetchItems={fetchItems}
+                setShowDetails={setShowDetails}
+              />
+            )}
+          </>
+        )}
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
